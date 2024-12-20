295722556f7dd8a3209581deebf23d211f6dac37
Subekshya <subekshyakayastha4@gmail.com>
2024-12-20 21:02:38 +0545

    add themes

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:furever_home/core/app_theme/app_theme.dart';
-import 'package:furever_home/view/dashboard_view.dart';
+import 'package:furever_home/view/onboard_view.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const DashboardView(),
+      home: const OnboardView(),
       theme: getApplicationTheme(),
     );
   }
