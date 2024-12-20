be364a4ae24cdd559efd2f8993577cad516116ec
Subekshya <subekshyakayastha4@gmail.com>
2024-12-20 19:59:58 +0545

    add navigation bar screens and logo

--- lib/view/dashboard_view.dart
+++ lib/view/dashboard_view.dart
@@ -11,59 +11,92 @@ class DashboardView extends StatefulWidget {
 }
 
 class _DashboardViewState extends State<DashboardView> {
-  int _selectedIndex = 0;
   List<Widget> lstBottomScreen = [
     const HomeScreen(),
     const ChatScreen(),
     const ProfileView(),
   ];
 
+  int _currentIndex = 0;
+
+  void _onTabSelected(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Welcome",
-            style: TextStyle(
-              fontSize: 24,
-              color: Colors.white,
-            )),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Image.asset(
+            'assets/images/fureverHome_logo.png',
+            height: 100,
+            width: 100,
+          ),
+        ),
+        title: const Text(
+          "Welcome",
+        ),
+        // actions: const [
+        //   Padding(
+        //     padding: EdgeInsets.only(right: 20),
+        //     child: Row(
+        //       children: [
+        //         Icon(Icons.search),
+        //         SizedBox(width: 16), // Space between the icons
+        //         Icon(Icons.notifications),
+        //       ],
+        //     ),
+        //   ),
+        // ],
+        centerTitle: false, // Align the title to the left
       ),
-      // body: const Center(
-      //   child: Column(
-      //     children: [
-      //       Text("This is a dashboard page",
-      //           style: TextStyle(
-      //             fontSize: 24,
-      //           )),
-      //       PetProfileCardView(),
-      //     ],
-      //   ),
-      // ),
-      body: lstBottomScreen[_selectedIndex],
+      body: lstBottomScreen[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
         showSelectedLabels: false,
         showUnselectedLabels: false,
-        items: const [
+        backgroundColor: const Color(0xFFFCDDC9),
+        // selectedItemColor: const Color(0xFF66AEA6),
+        selectedItemColor: const Color(0xCC96614D),
+        elevation: 8,
+        items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+            icon: _buildIcon(0, Icons.home),
             label: "Home",
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.chat_bubble),
+            icon: _buildIcon(1, Icons.chat_bubble),
             label: "Chat",
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
+            icon: _buildIcon(2, Icons.person),
             label: "Profile",
           ),
         ],
-        currentIndex: _selectedIndex,
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
+        onTap: _onTabSelected,
       ),
+    );
+  }
+
+  Widget _buildIcon(int index, IconData icon) {
+    final bool isSelected = _currentIndex == index;
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(icon),
+        if (isSelected)
+          Container(
+            margin: const EdgeInsets.only(top: 4),
+            height: 2,
+            width: 24,
+            color: const Color(0xFF66AEA6), // Line color
+          ),
+      ],
     );
   }
 }
