0ef156307b22a010df9be02875c88115d57eaaff
Subekshya <subekshyakayastha4@gmail.com>
2024-12-20 16:34:02 +0545

    added bottom navigation

--- lib/view/dashboard_view.dart
+++ lib/view/dashboard_view.dart
@@ -1,17 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:furever_home/view/chat_screen.dart';
+import 'package:furever_home/view/home_screen.dart';
+import 'package:furever_home/view/profile_view.dart';
 
-class DashboardView extends StatelessWidget {
+class DashboardView extends StatefulWidget {
   const DashboardView({super.key});
+
+  @override
+  State<DashboardView> createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+  int _selectedIndex = 0;
+  List<Widget> lstBottomScreen = [
+    const HomeScreen(),
+    const ChatScreen(),
+    const ProfileView(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Welcome"),
+        title: const Text("Welcome",
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.white,
+            )),
       ),
       // body: const Center(
-      //   child: Text("This is a dashboard page"),
+      //   child: Column(
+      //     children: [
+      //       Text("This is a dashboard page",
+      //           style: TextStyle(
+      //             fontSize: 24,
+      //           )),
+      //       PetProfileCardView(),
+      //     ],
+      //   ),
       // ),
+      body: lstBottomScreen[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat_bubble),
+            label: "Chat",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
     );
   }
 }
