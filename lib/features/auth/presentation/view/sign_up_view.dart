9e8aa59f2ecfc9d1c92e7221c12e2660cbcecbaf
Subekshya <subekshyakayastha4@gmail.com>
2025-01-16 22:55:57 +0545

    register using bloc and hive

---
+++ lib/features/auth/presentation/view/sign_up_view.dart
@@ -0,0 +1,336 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:furever_home/core/common/snackbar/my_snackbar.dart';
+import 'package:furever_home/features/auth/presentation/view/sign_in_view.dart';
+import 'package:furever_home/features/auth/presentation/view_model/signup/register_bloc.dart';
+
+class SignUpView extends StatefulWidget {
+  const SignUpView({super.key});
+
+  @override
+  State<SignUpView> createState() => _SignUpViewState();
+}
+
+class _SignUpViewState extends State<SignUpView> {
+  final _key = GlobalKey<FormState>();
+  final _fnameController = TextEditingController(text: 'subekshya');
+  final _lnameController = TextEditingController(text: 'kayastha');
+  final _phoneController = TextEditingController(text: '123456789');
+  final _usernameController = TextEditingController(text: 'subekshya');
+  final _passwordController = TextEditingController(text: 'password123');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Form(
+              key: _key,
+              child: Column(
+                children: [
+                  const Text(
+                    "Create New Account",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF96614D)),
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _fnameController,
+                    keyboardType: TextInputType.text,
+                    style: const TextStyle(
+                      fontFamily: 'WorkSansSemiBold',
+                      fontSize: 16.0,
+                      color: Colors.black,
+                    ),
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      prefixIcon: const Icon(
+                        Icons.person_2_outlined,
+                        color: Color(0xCC96614D),
+                        size: 22.0,
+                      ),
+                      labelText: 'First Name',
+                    ),
+                    validator: ((value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter first name';
+                      }
+                      return null;
+                    }),
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _lnameController,
+                    keyboardType: TextInputType.text,
+                    style: const TextStyle(
+                      fontFamily: 'WorkSansSemiBold',
+                      fontSize: 16.0,
+                      color: Colors.black,
+                    ),
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      prefixIcon: const Icon(
+                        Icons.person_2_outlined,
+                        color: Color(0xCC96614D),
+                        size: 22.0,
+                      ),
+                      labelText: 'Last Name',
+                    ),
+                    validator: ((value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter last name';
+                      }
+                      return null;
+                    }),
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _phoneController,
+                    keyboardType: TextInputType.text,
+                    style: const TextStyle(
+                      fontFamily: 'WorkSansSemiBold',
+                      fontSize: 16.0,
+                      color: Colors.black,
+                    ),
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      prefixIcon: const Icon(
+                        Icons.call,
+                        color: Color(0xCC96614D),
+                        size: 22.0,
+                      ),
+                      labelText: 'Mobile Number',
+                    ),
+                    validator: ((value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter phoneNo';
+                      }
+                      return null;
+                    }),
+                  ),
+                  const SizedBox(height: 20),
+                  // TextFormField(
+                  //   keyboardType: TextInputType.emailAddress,
+                  //   style: const TextStyle(
+                  //     fontFamily: 'WorkSansSemiBold',
+                  //     fontSize: 16.0,
+                  //     color: Colors.black,
+                  //   ),
+                  //   decoration: InputDecoration(
+                  //     enabledBorder: OutlineInputBorder(
+                  //       borderRadius: BorderRadius.circular(20),
+                  //     ),
+                  //     focusedBorder: OutlineInputBorder(
+                  //       borderRadius: BorderRadius.circular(20),
+                  //     ),
+                  //     prefixIcon: const Icon(
+                  //       Icons.email,
+                  //       color: Color(0xCC96614D),
+                  //       size: 22.0,
+                  //     ),
+                  //     labelText: 'Email Address',
+                  //   ),
+                  // ),
+                  // const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _usernameController,
+                    keyboardType: TextInputType.text,
+                    style: const TextStyle(
+                      fontFamily: 'WorkSansSemiBold',
+                      fontSize: 16.0,
+                      color: Colors.black,
+                    ),
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      prefixIcon: const Icon(
+                        Icons.calendar_today,
+                        color: Color(0xCC96614D),
+                        size: 22.0,
+                      ),
+                      labelText: 'Username',
+                      // hintText: 'YYYY-MM-DD',
+                    ),
+                    validator: ((value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter username';
+                      }
+                      return null;
+                    }),
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    style: const TextStyle(
+                      fontFamily: 'WorkSansSemiBold',
+                      fontSize: 16.0,
+                      color: Colors.black,
+                    ),
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      prefixIcon: const Icon(
+                        Icons.lock,
+                        size: 22.0,
+                        color: Color(0xCC96614D),
+                      ),
+                      labelText: 'Password',
+                    ),
+                    validator: ((value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter password';
+                      }
+                      return null;
+                    }),
+                  ),
+                  // const SizedBox(height: 20),
+                  // TextFormField(
+                  //   obscureText: true,
+                  //   style: const TextStyle(
+                  //     fontFamily: 'WorkSansSemiBold',
+                  //     fontSize: 16.0,
+                  //     color: Colors.black,
+                  //   ),
+                  //   decoration: InputDecoration(
+                  //     enabledBorder: OutlineInputBorder(
+                  //       borderRadius: BorderRadius.circular(20),
+                  //     ),
+                  //     focusedBorder: OutlineInputBorder(
+                  //       borderRadius: BorderRadius.circular(20),
+                  //     ),
+                  //     prefixIcon: const Icon(
+                  //       Icons.lock_clock,
+                  //       size: 22.0,
+                  //       color: Color(0xCC96614D),
+                  //     ),
+                  //     labelText: 'Confirm Password',
+                  //   ),
+                  // ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all<Color>(
+                            const Color(0xFF66AEA6)),
+                        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        padding: WidgetStateProperty.all<EdgeInsets>(
+                            const EdgeInsets.symmetric(vertical: 15)),
+                      ),
+                      // onPressed: () {
+                      //   Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(builder: (context) => LoginView()),
+                      //   );
+                      // },
+                      onPressed: () {
+                        if (_key.currentState!.validate()) {
+                          context.read<RegisterBloc>().add(
+                                RegisterStudent(
+                                  fname: _fnameController.text,
+                                  lname: _lnameController.text,
+                                  password: _passwordController.text,
+                                  username: _usernameController.text,
+                                  context: context,
+                                ),
+                              );
+                          // NavigateToHomeScreenEvent(
+                          //   context: context,
+                          //   destination: LoginView(),
+                          // );
+                        } else {
+                          showMySnackBar(
+                            context: context,
+                            message: 'Could not Register',
+                            color: Colors.red,
+                          );
+                        }
+                      },
+                      child: const Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Color(0xFFFFFFFF),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    "Already Have An Account? ",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    width: double.infinity,
+                    child: OutlinedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            WidgetStateProperty.all<Color>(Colors.transparent),
+                        // transparent background for outlined button
+                        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.circular(30), // Blue border
+                          ),
+                        ),
+                        padding: WidgetStateProperty.all<EdgeInsets>(
+                          const EdgeInsets.symmetric(vertical: 15),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => LoginView()),
+                        );
+                      },
+                      child: const Text(
+                        "Log In",
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Color(
+                                0xFF66AEA6)), // Text color matching button border
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
