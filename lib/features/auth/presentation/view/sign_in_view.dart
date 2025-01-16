748c1e5e154dc132e9fb3d1e76b1d71ce4666e99
Subekshya <subekshyakayastha4@gmail.com>
2025-01-16 22:55:41 +0545

    login using bloc and hive

---
+++ lib/features/auth/presentation/view/sign_in_view.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:furever_home/features/auth/presentation/view/sign_up_view.dart';
+import 'package:furever_home/features/auth/presentation/view_model/login/login_bloc.dart';
+
+class LoginView extends StatelessWidget {
+  LoginView({super.key});
+
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _usernameController = TextEditingController();
+
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Form(
+          key: _formKey,
+          child: Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    BlocBuilder<LoginBloc, LoginState>(
+                      builder: (context, state) {
+                        return Text(
+                          'Login',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF96614D),
+                          ),
+                        );
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      key: const ValueKey('username'),
+                      controller: _usernameController,
+                      // keyboardType: TextInputType.emailAddress,
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                        color: Color(0xFF96614D),
+                      ),
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.person,
+                          color: Color(0xCC96614D),
+                          size: 22.0,
+                        ),
+                        labelText: 'Username',
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter username';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      key: const ValueKey('password'),
+                      controller: _passwordController,
+                      obscureText: true,
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                        color: Color(0xFF96614D),
+                      ),
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          size: 22.0,
+                          color: Color(0xCC96614D),
+                        ),
+                        labelText: 'Password',
+                      ),
+                      validator: ((value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter password';
+                        }
+                        return null;
+                      }),
+                    ),
+                    const SizedBox(height: 10),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).pushNamed("/forget-password");
+                        },
+                        child: Text(
+                          "Forgot password?",
+                          style: TextStyle(color: Colors.grey.shade800),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        // onPressed: _handleLogin,
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            context.read<LoginBloc>().add(
+                                  LoginStudentEvent(
+                                    context: context,
+                                    username: _usernameController.text,
+                                    password: _passwordController.text,
+                                  ),
+                                );
+                          }
+                        },
+                        child: const Text(
+                          "Log In",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xFFFFFFFF),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      "Don't Have An Account? ",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      width: double.infinity,
+                      child: OutlinedButton(
+                        key: const ValueKey("registerButton"),
+                        style: ButtonStyle(
+                          shape:
+                              WidgetStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                          ),
+                          padding: WidgetStateProperty.all<EdgeInsets>(
+                            const EdgeInsets.symmetric(vertical: 15),
+                          ),
+                        ),
+                        onPressed: () {
+                          context.read<LoginBloc>().add(
+                                NavigateRegisterScreenEvent(
+                                  destination: SignUpView(),
+                                  context: context,
+                                ),
+                              );
+                          // Navigator.push(
+                          //   context,
+                          //   MaterialPageRoute(
+                          //       builder: (context) => const SignUpView()),
+                          // );
+                        },
+                        child: const Text(
+                          "Sign Up",
+                          style:
+                              TextStyle(fontSize: 20, color: Color(0xFF66AEA6)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
